39449536c399d296bbc8c818955f19f85dfbef16
Sehgal sehgal <usehgal417@gmail.com>
2021-04-13 14:58:09 +0530

    Add project files.

---
+++ CrudA/WebForm1.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CrudA.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+			<table>
+				<tr>
+					<td>Name:</td>
+					<td><asp:TextBox ID="txt1" runat="server"></asp:TextBox></td>
+
+				</tr>
+				<tr>
+					<td>Blood Group:</td>
+					<td><asp:RadioButtonList ID="rbl1" runat="server" RepeatColumns="7">
+					
+
+					    </asp:RadioButtonList></td>
+
+				</tr>
+				<tr>
+					<td>Course:</td>
+					<td><asp:DropDownList ID="ddl1" runat="server">
+						
+					    </asp:DropDownList></td>
+
+				</tr>
+				<tr>
+					<td></td>
+					<td>
+						<asp:Button ID="btnsave" runat="server" Text="save" OnClick="btnsave_Click" />
+					</td>
+				</tr>
+
+				<tr>
+					<td></td>
+					<td>
+						<asp:GridView ID="grid1" runat="server"></asp:GridView>
+					</td>
+
+				</tr>
+			</table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CrudA/WebForm1.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace CrudA
+{
+	public partial class WebForm1 : System.Web.UI.Page
+
+	{
+		SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString);
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			bindgrid();
+			bloodgroup();
+			Corsename();
+		}
+		public void bindgrid()
+		{
+			con.Open();
+			SqlCommand cmd = new SqlCommand("sp_Register_get", con);
+			cmd.CommandType = CommandType.StoredProcedure;
+			SqlDataAdapter da = new SqlDataAdapter(cmd);
+			DataTable dt = new DataTable();
+			da.Fill(dt);
+			con.Close();
+			grid1.DataSource = dt;
+			grid1.DataBind();
+		}
+
+		public void bloodgroup()
+		{
+			con.Open();
+			SqlCommand cmd = new SqlCommand("select * from bloodgroup", con);
+			SqlDataAdapter da = new SqlDataAdapter(cmd);
+			DataTable dt = new DataTable();
+			da.Fill(dt);
+			con.Close();
+			rbl1.DataValueField = "bid";
+			rbl1.DataTextField = "bname";
+			rbl1.DataSource = dt;
+			rbl1.DataBind();
+		}
+
+		public void Corsename()
+		{
+			con.Open();
+			SqlCommand cmd = new SqlCommand("select * from Course", con);
+			SqlDataAdapter da = new SqlDataAdapter(cmd);
+			DataTable dt = new DataTable();
+			da.Fill(dt);
+			con.Close();
+			ddl1.DataValueField = "cid";
+			ddl1.DataTextField = "cname";
+			ddl1.DataSource = dt;
+			ddl1.DataBind();
+			ddl1.Items.Insert(0,new ListItem("--Select--","0"));
+
+		}
+
+
+		protected void btnsave_Click(object sender, EventArgs e)
+		{
+			con.Open();
+			SqlCommand cmd = new SqlCommand("sp_Register_insert", con);
+			cmd.CommandType = CommandType.StoredProcedure;
+			cmd.Parameters.AddWithValue("@name", txt1.Text);
+			cmd.Parameters.AddWithValue("@bloodgroup", ddl1.SelectedValue);
+			cmd.Parameters.AddWithValue("@course", rbl1.SelectedValue);
+			cmd.ExecuteNonQuery();
+			con.Close();
+			bindgrid();
+		}
+	}
+}
